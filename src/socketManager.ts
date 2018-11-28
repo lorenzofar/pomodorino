b923c7037943c0120ac079d3512f7d436722a755
Lorenzo Farinelli <lorenzo.farinelli@outlook.it>
2018-11-28 16:15:06 +0100

    Add initialization method and connection event handlers to socket manager

--- src/socketManager.ts
+++ src/socketManager.ts
@@ -6,24 +6,49 @@
 
 import * as socket_io from "socket.io-client";
 
-const SOCKET_URL: string = "https://necstelectra.herokuapp.com";
+const SOCKET_URL: string = "http://localhost:3000";
 
-class SocketManager{
-    private static io = null;    
+class SocketManager {
+    private static io: SocketIOClient.Socket = null;
 
-    public static connect(){
-        // Before doing anything, check connection status
-        //TODO: Retrieve credentials and build handshake message        
+    //TODO: Hook module to credentials manager to fire an event 
+    // when credentials change and reconnect the socket accordingly
+
+    public static initialize() {
+        let email: string;
+        let password: string;
+        let qs = `email=${email}&password=${password}&mode=provider`;
+        this.io = socket_io(SOCKET_URL, { autoConnect: false, query: qs });
     }
 
-    public static disconnect(){
+    public static connect() {
         // Before doing anything, check connection status
+        //TODO: Retrieve credentials and build handshake message  
+        if (this.io == null) return;
+
+        this.io.open();
+
+        this.io.on("connect", () => {
+            console.log("Socket connected");
+        });
+        this.io.on("disconnect", () => {
+            console.log("Socket disconnected");
+        });
+
     }
 
-    public static get connectionStatus(){
-        return null;
+    public static disconnect() {
+        // Before doing anything, check connection status
+        this.io.disconnect();
+        this.io.close();
+
+    }
+
+    public static get connectionStatus() {
+        return this.io.connected;
     }
 }
 
+SocketManager.initialize();
 
 export default SocketManager;
