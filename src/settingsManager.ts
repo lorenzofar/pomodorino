99b2ad1b39eeca9f7408762bc0184fb86395750d
Lorenzo Farinelli <lorenzo.farinelli@outlook.it>
2018-11-30 18:46:46 +0100

    Add method to read from config file
    Added concurrency handling using mutexes to avoid reading before writing
    Added 'locks' package

--- src/settingsManager.ts
+++ src/settingsManager.ts
@@ -1,4 +1,5 @@
 import * as fs from "fs";
+import * as locks from "locks";
 
 const CONFIG_FILE_NAME = "config.json";
 
@@ -13,20 +14,50 @@ interface Config {
 }
 
 class SettingsManager {
-    private static configExists = (): boolean => fs.existsSync(`./${CONFIG_FILE_NAME}`);
+    private static configExists(): boolean {
+        return fs.existsSync(`./${CONFIG_FILE_NAME}`);
+    }
+
+    private static initMutex: locks.Mutex;
 
     public static initialize() {
-        if (!this.configExists()) {
-            this.createBlankConfig();
-        };
+        this.initMutex = new locks.Mutex();
+
+        /* ===== BINDINGS ===== */
+        this.touchConfigFile = this.touchConfigFile.bind(this);
+        this.readConfigFile = this.readConfigFile.bind(this);
+        this.writeConfigFile = this.writeConfigFile.bind(this);
+
+        // Manage concurrency
+        this.initMutex.lock(this.touchConfigFile);
+        this.initMutex.lock(this.readConfigFile);
+
         //TODO: Continue initialization
     }
 
     /* ===== CONFIG FILE MANAGEMENT ===== */
 
+    private static touchConfigFile() {
+        if (!this.configExists()) this.createBlankConfig();
+        else this.initMutex.unlock();
+    }
+
     private static writeConfigFile(config: Config) {
         let jsonConfig = JSON.stringify(config);
-        fs.writeFile(`./${CONFIG_FILE_NAME}`, jsonConfig, "utf8", () => console.log(`[SETTINGS] Succesfully written config file`));
+        fs.writeFile(`./${CONFIG_FILE_NAME}`, jsonConfig, "utf8", () => {
+            console.log("[SETTINGS] written config file");
+            this.initMutex.unlock();
+        });
+    }
+
+    private static readConfigFile(): Config {
+        // The function always provides a configuration object
+        // If the file is not present, it is created and then returned
+
+        let rawData = fs.readFileSync(`./${CONFIG_FILE_NAME}`, "utf8");
+        let data: Config = JSON.parse(rawData); // Parse JSON string
+        this.initMutex.unlock();
+        return data;
     }
 
     private static createBlankConfig() {
@@ -44,7 +75,5 @@ class SettingsManager {
         this.writeConfigFile(blankConfig);
     }
 }
-
-SettingsManager.initialize();
 
 export default SettingsManager;
