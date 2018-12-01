a695b11dab2ed154bd6d594a2e472140513d03a3
Lorenzo Farinelli <lorenzo.farinelli@outlook.it>
2018-12-01 12:05:39 +0100

    Add dataPoint model

--- src/dataRetriever.ts
+++ src/dataRetriever.ts
@@ -2,15 +2,9 @@
  * This module provides data to the analyzer
  */
 
- //FIXME: Change interface to reflect real structure of data to retrieve
-interface dataPoint{
-    light: number;
-    noise: number;
-}
-
 class DataRetriever {
     private static readData(){
-        
+
     }
 
     public static get data(){
---
+++ src/models/dataPoint.ts
@@ -0,0 +1,8 @@
+interface sensorsData {
+    [sensor: string]: number;
+}
+
+export default interface DataPoint {
+    timestamp: number; // Hold UNIX timestamp
+    data: sensorsData; // Hold values retrieved by sensors
+}
