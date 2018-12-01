3d40b41c57b1a22cc5312c6fef491bdbbd63706c
Lorenzo Farinelli <lorenzo.farinelli@outlook.it>
2018-12-01 23:36:02 +0100

    Subscribe socket manager to mode manager
    Also added logging when opening or closing a socket connection

--- src/socketManager.ts
+++ src/socketManager.ts
@@ -6,7 +6,12 @@
 
 import * as socket_io from "socket.io-client";
 
+import { modes } from "./models/modes";
+import ModeManager from "./modeManager";
+
 const SOCKET_URL: string = "http://localhost:3000";
+
+const ID = "socketmanager";
 
 class SocketManager {
     private static io: SocketIOClient.Socket = null;
@@ -14,11 +19,34 @@ class SocketManager {
     //TODO: Hook module to credentials manager to fire an event 
     // when credentials change and reconnect the socket accordingly
 
+    //TODO: During initialization, socket manager should understand in which the 
+    // device is working in and start the websocket if needed
+
     public static initialize() {
+
+        /* ===== BINDINGS ===== */
+
+        this.handleModeChange = this.handleModeChange.bind(this);
+
         let email: string;
         let password: string;
         let qs = `username=${email}&password=${password}&mode=provider`;
         this.io = socket_io(SOCKET_URL, { autoConnect: false, query: qs });
+
+        // Subscribe to mode manager and listen to changes
+        let subscriptionResult = ModeManager.subscribe({ id: ID, handler: this.handleModeChange });
+
+        // Subscribe to mode changes
+    }
+
+    private static handleModeChange(mode: modes) {
+        //TODO: Connect / disconnect socket accordingly
+        if (mode == modes.STREAM) {
+            this.connect();
+        }
+        else {
+            this.disconnect();
+        }
     }
 
     public static connect() {
@@ -26,8 +54,10 @@ class SocketManager {
         //TODO: Retrieve credentials and build handshake message  
         if (this.io == null) return;
 
+        console.log("[SOCKET] opening connection")
+
         this.io.open();
-        
+
         this.io.on("connect", () => {
             console.log("Socket connected");
         });
@@ -38,10 +68,11 @@ class SocketManager {
     }
 
     public static disconnect() {
+        console.log("[SOCKET] closing connection");
+
         // Before doing anything, check connection status
         this.io.disconnect();
         this.io.close();
-
     }
 
     public static get connectionStatus() {
