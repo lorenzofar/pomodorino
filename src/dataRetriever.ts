63609c69acb5010ee5bf53568406d04110226fc7
Lorenzo Farinelli <lorenzo.farinelli@outlook.it>
2018-12-05 10:56:52 +0100

    Add sample data retrieving to dataRetriever

--- src/dataRetriever.ts
+++ src/dataRetriever.ts
@@ -1,8 +1,15 @@
+import * as moment from "moment";
+import DataPoint from "./models/dataPoint";
+
 /**
  * This module provides data to the analyzer
  */
 
 class DataRetriever {
+
+    private static dataCache: DataPoint = null;
+
+
     public static initialize(){
         console.log("[RETRIEVER] initializing");
         this.readData = this.readData.bind(this);
@@ -10,13 +17,28 @@ class DataRetriever {
 
     private static readData(){
         // Read data from sensors and update cache
+        //TODO: Pass data point to dbManager
+        // TODO: Pass data point to socket manager
+        let now = moment(); // Get current datetime
+        let dp: DataPoint = {
+            timestamp: now.unix(),
+            data: {
+                "light": 34,
+                "noise": 31
+            }
+        }
+        this.dataCache = dp;
+
     }
 
-    public static get data(){
-        // Refresh data
+    public static get data(): DataPoint{
+        /**
+         * This method triggers an update of the data cache and return its content
+         * FIXME: This could however bring issues as it would not provide the most recent data point 
+         * but instead the (N-1)th
+         */
         this.readData();
-        // send data back to those who requested
-        return null;
+        return this.dataCache;
     }
 }
 
