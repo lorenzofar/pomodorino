eacd293af83410cd3bf1041f344c275f3d8ee1ae
Lorenzo Farinelli <lorenzo.farinelli@outlook.it>
2018-12-15 12:06:20 +0100

    Add data insertion and emission in cron ticks

--- src/orchestrator.ts
+++ src/orchestrator.ts
@@ -4,14 +4,18 @@ import ModeManager from "./modeManager";
 import { modes } from "./models/modes";
 
 import DataRetriever from "./dataRetriever";
+import DbManager from "./dbManager";
+import CredentialsManager from "./credentialsManager";
+import SocketManager from "./socketManager";
 
-const CLOCK = 2; // expressed in seconds
+const { CLOCK = 2 } = process.env; // expressed in seconds
 const CRON_SCHEDULE = `*/${CLOCK} * * * * *`;
 
 class Orchestrator {
     private static clockJob: CronJob;
 
     private static tickCount: number;
+    private static username: string;
 
     public static initialize() {
         console.log("[CLOCK] initializing");
@@ -19,6 +23,7 @@ class Orchestrator {
         this.tick = this.tick.bind(this);
         this.clockJob = new CronJob(CRON_SCHEDULE, this.tick);
         this.clockJob.start();
+        CredentialsManager.getCredentials(credentials => this.username = credentials.username);
     }
 
     public static get jobStatus(): boolean {
@@ -28,6 +33,8 @@ class Orchestrator {
     private static tick() {
         //TODO: Read data from sensors
         let data = DataRetriever.data;
+        DbManager.insertDataPoint(this.username, data);
+        if (ModeManager.mode === modes.STREAM) SocketManager.emitDataPoint(data);
         //TODO: Call analyzer function
         //TODO: Pack everything
         //TODO: Check if the socket is active and send
